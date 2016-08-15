ed6b172bdd692e376c91d33043bd3f5a0142ff1a
nicolasferry <nicolas.ferry@sintef.no>
2016-08-15 17:36:51 +0200

    add load test

--- src/sensorSimulator.js
+++ src/sensorSimulator.js
@@ -1,18 +1,49 @@
-var config = require('./config.json');
-var fs = require('fs');  // file system
 var http = require('http');
+var config = require('./config.json');
+var fs = require('fs')
+    , util = require('util')
+    , stream = require('stream')
+    , es = require('event-stream');
+
+
+var lineNr = 0;
+var currentL = 0;
+
+function provideContent(res, nr){
+
+var s= fs.createReadStream(config.file)
+    .pipe(es.split())
+    .pipe(es.mapSync(function(line){
+
+	if(currentL >= lineNr){
+        // pause the readstream
+        s.pause();
+
+        // process line here and call s.resume() when rdy
+        lineNr++;
+	console.log(lineNr);
+        res.write(line+"\n");
+
+        setTimeout(function(){
+                s.resume();
+        }, config.frequency);
+	}
+	currentL++;
+    })
+
+    .on('error', function(){
+        console.log('Error while reading file.');
+    })
+
+    .on('end', function(){
+        console.log('Read entire file.')
+	process.exit();
+    })
+);
+}
+
 var server = http.createServer(function (req, res) {
-  // logic here to determine what file, etc
-  setInterval( function(){
-	  var rstream = fs.createReadStream(config.file);
-	  //rstream.pipe(res);
-	  rstream.on('data', function(chunk) {
-		    res.write(chunk);
-		});
-
-		rstream.on('end', function() {
-
-		});
-  }, config.frequency)
+	provideContent(res);
 });
-server.listen(8000, config.ip);  // start
+
+server.listen(8000);  // start
